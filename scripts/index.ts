bf8d682e56821540db6fd366f89cadcff036e578
neonew <neonew@gmail.com>
2022-08-07 19:02:42 +0200

    Drop own TypeScript example
    toastify replaces it

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,9 +1,5 @@
 import Toastify from 'toastify-js'
 
-const elemDiv: HTMLElement = document.createElement('div');
-elemDiv.innerHTML = '<div>Appended Element by TypeScript</div>';
-document.body.appendChild(elemDiv);
-
 Toastify({
   text: "This is a toast",
   duration: 3000,
