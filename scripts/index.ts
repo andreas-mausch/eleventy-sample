803b2a57a89c9961886f5d524dfdb129707ecd05
neonew <neonew@gmail.com>
2022-08-07 19:02:07 +0200

    Use toastify-js to demonstrate js and css imports

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,3 +1,12 @@
+import Toastify from 'toastify-js'
+
 const elemDiv: HTMLElement = document.createElement('div');
 elemDiv.innerHTML = '<div>Appended Element by TypeScript</div>';
 document.body.appendChild(elemDiv);
+
+Toastify({
+  text: "This is a toast",
+  duration: 3000,
+  gravity: "top", // `top` or `bottom`
+  position: "left", // `left`, `center` or `right`
+}).showToast();
