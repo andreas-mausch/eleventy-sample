e00f199c853852aba85ef0a76987fd58ccd8bf5e
neonew <neonew@gmail.com>
2022-08-07 15:54:51 +0200

    Add first JavaScript file

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("posts/*/*.{jpg,jpeg,png,svg}");
+  eleventyConfig.addPassthroughCopy("scripts/*.js");
 
   eleventyConfig.addLayoutAlias("page", "layouts/page.njk");
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+var elemDiv = document.createElement('div');
+elemDiv.innerHTML = '<div>Appended Element by JavaScript</div>';
+document.body.appendChild(elemDiv);
