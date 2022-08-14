6cbe4a1d70c9640297d4067221e745cb16748438
neonew <neonew@gmail.com>
2022-08-14 07:19:17 +0200

    Reference to source for inline code blocks fix

--- eleventy/markdown.js
+++ eleventy/markdown.js
@@ -5,6 +5,8 @@ const markdown = markdownIt({
   html: true,
 }).use(markdownItAnchor)
 
+// Unfortunately, inline code blocks are not processed by default, so we need this.
+// Compare to https://github.com/11ty/eleventy-plugin-syntaxhighlight/issues/38#issuecomment-1022305948
 markdown.renderer.rules.code_inline = (tokens, idx, { langPrefix = "" }) => {
   const token = tokens[idx]
   return `<code class="${langPrefix}">${token.content}</code>`
