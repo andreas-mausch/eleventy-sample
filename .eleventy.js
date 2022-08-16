d16005bb775cf705c9ed22de533497513761fc98
neonew <neonew@gmail.com>
2022-08-16 22:41:11 +0200

    Use liquid tag instead of shortcode to validate link-post target

--- .eleventy.js
+++ .eleventy.js
@@ -23,7 +23,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLiquidShortcode("thumbnail", imageShortcodes.thumbnail)
   eleventyConfig.addLiquidShortcode("thumbnail-clickable", imageShortcodes.clickableThumbnail)
   eleventyConfig.addLiquidShortcode("carousel", imageShortcodes.carousel)
-  eleventyConfig.addLiquidShortcode("link-post", linkPost)
+  eleventyConfig.addPlugin(linkPost)
 
   eleventyConfig.addPlugin(eleventySass, {
     sass: {
--- eleventy/link-post.js
+++ eleventy/link-post.js
@@ -3,6 +3,20 @@
 // And here: https://github.com/11ty/eleventy/issues/1641
 const getPathPrefix = () => process.env.ELEVENTY_PATH_PREFIX || ""
 
-module.exports = href => {
-  return `${getPathPrefix()}/posts/${href}/`
+module.exports = function (eleventyConfig, _options = {}) {
+  eleventyConfig.addLiquidTag("link-post", _liquidEngine => ({
+    parse: function (tagToken, _remainingTokens) {
+      this.linkToPost = tagToken.args
+    },
+    render: async function (scope, _hash) {
+      const linkToPost = await this.liquid.evalValue(this.linkToPost, scope)
+      const post = scope.environments.collections.posts.find(post => post.url === `/posts/${linkToPost}/`)
+
+      if (!post) {
+        throw new Error(`link-post: Post not found ${linkToPost}`)
+      }
+
+      return `${getPathPrefix()}${post.url}`
+    }
+  }))
 }
