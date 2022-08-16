86ba7f7f059076e8a118542c902ce9db2ccfa0d9
neonew <neonew@gmail.com>
2022-08-17 00:38:10 +0200

    Show anchor after heading

--- eleventy/markdown.js
+++ eleventy/markdown.js
@@ -1,5 +1,5 @@
 const markdownIt = require("markdown-it")
-const markdownItAnchor = require("markdown-it-anchor")
+const anchor = require("markdown-it-anchor")
 const externalLinks = require("markdown-it-external-links")
 const prism = require("markdown-it-prism")
 const hierarchy = require("./markdown-it-hierarchy")
@@ -8,7 +8,10 @@ const markdown = markdownIt({
   html: true
 }).use(hierarchy)
   .use(externalLinks)
-  .use(markdownItAnchor)
+  .use(anchor, { permalink: anchor.permalink.ariaHidden({
+    symbol: "<svg viewBox=\"0 0 16 16\" version=\"1.1\" width=\"16\" height=\"16\" aria-hidden=\"true\"><path fill-rule=\"evenodd\" d=\"M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z\"></path></svg>",
+    placement: "after"
+  }) })
   .use(prism, { defaultLanguage: "plain" })
 
 // Unfortunately, inline code blocks are not processed by default, so we need this.
