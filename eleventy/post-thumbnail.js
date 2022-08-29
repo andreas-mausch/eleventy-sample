f9ef99cbda0db2e7d67d82ee550f12aa3a1720a9
neonew <neonew@gmail.com>
2022-08-29 11:56:00 +0200

    Postlist styling

--- eleventy/post-thumbnail.js
+++ eleventy/post-thumbnail.js
@@ -5,8 +5,7 @@ module.exports = post => {
   if (post.inputPath && post.inputPath.endsWith("/index.md")) {
     const thumbnailPath = imageShortcodes.relativeFile("thumbnail.jpg", post)
     if (fs.existsSync(thumbnailPath)) {
-      const img = imageShortcodes.thumbnail("thumbnail.jpg", "Post thumbnail", post)
-      return `<div class="postlist-thumbnail">${img}</div>`
+      return imageShortcodes.thumbnail("thumbnail.jpg", "Post thumbnail", post)
     }
   }
   return ""
