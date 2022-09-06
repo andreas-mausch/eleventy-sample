f26d0b73015b2d3200dd681a46fc85a04d37050b
neonew <neonew@gmail.com>
2022-09-06 21:04:02 +0200

    Throw error if thumbnail was defined but not found

--- eleventy/post-thumbnail.js
+++ eleventy/post-thumbnail.js
@@ -5,9 +5,13 @@ module.exports = post => {
   if (post.inputPath && post.inputPath.endsWith("/index.md")) {
     const filename = post.data.thumbnail || "thumbnail.jpg"
     const thumbnailPath = imageShortcodes.relativeFile(filename, post)
-    if (fs.existsSync(thumbnailPath)) {
-      return imageShortcodes.thumbnail(filename, "Post thumbnail", post)
+    if (!fs.existsSync(thumbnailPath)) {
+      if (post.data.thumbnail) {
+        throw new Error(`Thumbnail defined but not found: ${post.data.thumbnail} (${thumbnailPath})`)
+      } else {
+        return ""
+      }
     }
+    return imageShortcodes.thumbnail(filename, "Post thumbnail", post)
   }
-  return ""
 }
