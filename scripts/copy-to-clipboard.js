b182417ad606616f7618f5966d0ac475c6734070
andreas-mausch <andreas-mausch@users.noreply.github.com>
2022-12-14 18:50:00 +0000

    deploy: ed5187d7ecf1157570924fe4f80acdee8a6185fa

---
+++ scripts/copy-to-clipboard.js
@@ -0,0 +1,1 @@
+(()=>{function r(){document.querySelectorAll(".copy-to-clipboard").forEach(e=>e.addEventListener("click",o=>{let t=o.target.closest("code");navigator.clipboard.writeText(t.innerText)}))}})();
