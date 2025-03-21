41103575c9f8902ba45c72d53fe93dbfdf3a40d0
neonew <neonew@gmail.com>
2025-03-21 20:10:12 +0700

    eslint update: Convert config to new format
    It's a pity a good non-programmatic format like YAML is not supported as
    config anymore. :(
    
    globals needed to be added as dependency, because otherwise an outdated
    version was used which caused problems:
    https://github.com/sindresorhus/globals/issues/239

---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+import { defineConfig, globalIgnores } from "eslint/config"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import globals from "globals"
+import tsParser from "@typescript-eslint/parser"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+export default defineConfig([globalIgnores(["node_modules"]), {
+  extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+  plugins: {
+    "@typescript-eslint": typescriptEslint
+  },
+
+  languageOptions: {
+    globals: {
+      ...globals.browser
+    },
+
+    parser: tsParser,
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+    "arrow-parens": ["error", "as-needed"],
+    "arrow-spacing": "error",
+    "comma-dangle": ["error", "never"],
+    "space-in-parens": ["error", "never"],
+    "array-bracket-spacing": ["error", "never"],
+    "no-trailing-spaces": "error",
+
+    "space-before-function-paren": ["error", {
+      anonymous: "always",
+      named: "never",
+      asyncArrow: "always"
+    }],
+
+    "@typescript-eslint/no-unused-vars": ["error", {
+      argsIgnorePattern: "^_",
+      varsIgnorePattern: "^_",
+      caughtErrorsIgnorePattern: "^_"
+    }]
+  }
+}, {
+  files: ["**/eleventy.config.js", "eleventy/*.js"],
+
+  languageOptions: {
+    globals: {
+      ...globals.node
+    }
+  },
+
+  rules: {
+    "@typescript-eslint/no-var-requires": "off"
+  }
+}])
