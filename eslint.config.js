248cac692231e6d45e58c95c81eb10f762b4f9bd
neonew <neonew@gmail.com>
2025-03-21 21:11:14 +0700

    eslint: Ignore files from .gitignore
    See here:
    https://eslint.org/docs/latest/use/configure/ignore#including-gitignore-files

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
-import { defineConfig, globalIgnores } from "eslint/config"
+import { defineConfig } from "eslint/config"
+import { includeIgnoreFile } from "@eslint/compat"
 import typescriptEslint from "@typescript-eslint/eslint-plugin"
 import globals from "globals"
 import tsParser from "@typescript-eslint/parser"
@@ -9,63 +10,67 @@ import { FlatCompat } from "@eslint/eslintrc"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, ".gitignore")
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all
 })
 
-export default defineConfig([globalIgnores(["node_modules"]), {
-  extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  {
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
 
-  plugins: {
-    "@typescript-eslint": typescriptEslint
-  },
-
-  languageOptions: {
-    globals: {
-      ...globals.browser
+    plugins: {
+      "@typescript-eslint": typescriptEslint
     },
 
-    parser: tsParser,
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
 
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-    "arrow-parens": ["error", "as-needed"],
-    "arrow-spacing": "error",
-    "comma-dangle": ["error", "never"],
-    "space-in-parens": ["error", "never"],
-    "array-bracket-spacing": ["error", "never"],
-    "no-trailing-spaces": "error",
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
 
-    "space-before-function-paren": ["error", {
-      anonymous: "always",
-      named: "never",
-      asyncArrow: "always"
-    }],
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "never"],
+      "arrow-parens": ["error", "as-needed"],
+      "arrow-spacing": "error",
+      "comma-dangle": ["error", "never"],
+      "space-in-parens": ["error", "never"],
+      "array-bracket-spacing": ["error", "never"],
+      "no-trailing-spaces": "error",
 
-    "@typescript-eslint/no-unused-vars": ["error", {
-      argsIgnorePattern: "^_",
-      varsIgnorePattern: "^_",
-      caughtErrorsIgnorePattern: "^_"
-    }]
-  }
-}, {
-  files: ["**/eleventy.config.js", "eleventy/*.js"],
+      "space-before-function-paren": ["error", {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always"
+      }],
 
-  languageOptions: {
-    globals: {
-      ...globals.node
+      "@typescript-eslint/no-unused-vars": ["error", {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_"
+      }]
     }
-  },
+  }, {
+    files: ["**/eleventy.config.js", "eleventy/*.js"],
 
-  rules: {
-    "@typescript-eslint/no-var-requires": "off"
-  }
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
+
+    rules: {
+      "@typescript-eslint/no-var-requires": "off"
+    }
 }])
